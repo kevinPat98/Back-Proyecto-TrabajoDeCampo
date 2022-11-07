7a228eda532b35d96a22689249b4f7c6ac027077
Kevin Patiño <57466420+nico19989@users.noreply.github.com>
2022-11-06 20:06:09 -0500

    Integración Back

---
+++ models/Config.js
@@ -0,0 +1,12 @@
+'use strict'
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var ConfigSchema = Schema({
+    envio_activacion: {type: String, required: true},
+    monto_min_soles: {type: Number, required: true},
+    createdAt: {type:Date, default: Date.now, require: true}
+});
+
+module.exports =  mongoose.model('config',ConfigSchema);
